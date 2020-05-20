135793b3f78d1ef283e79a4a36ed9c974a2858a0
Cyrus Yip <cyruscmyip1@gmail.com>
2020-05-20 15:07:26 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# trianglify-generator
---
+++ script.js
@@ -0,0 +1,39 @@
+function generate() {
+  var colours = [];
+  $(".colour").each(function () {
+    colours.push($(this).val());
+  });
+  var pattern = Trianglify({
+    width: parseInt($("#width").val()),
+    height: parseInt($("#height").val()),
+    cell_size: parseInt($("#cell").val()),
+    x_colors: colours
+  });
+  $("canvas").last().replaceWith(pattern.canvas());
+}
+
+$(function () {
+  generate();
+  $("#form").change(generate);
+  $("#addcolour").click(function (e) {
+    e.preventDefault();
+    $(".colours").append(
+      "<div class='colourinput'><input type='color' class='colour' value='#" + Math.floor(Math.random()*16777215).toString(16) + "'><button class='x'>x</button></div>"
+    );
+    generate();
+    $(".x").click(function (e) {
+      e.preventDefault();
+      $(this).parent().remove();
+      generate();
+    });
+  });
+  $("#removeallcolour").click(function (e) {
+    e.preventDefault();
+    $(".colours").empty();
+  })
+  $(".x").click(function (e) {
+    e.preventDefault();
+    $(this).parent().remove();
+    generate();
+  });
+});
